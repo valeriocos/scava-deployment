bcdbc2f3b25df54a90039daae301548762e48d45
tdegueul <degueule@cwi.nl>
2018-08-08 12:53:50 +0200

    Revert "mongo-restore.sh: missing newline"
    This reverts commit 2f515ca8853abfc09cabe04597ac7429a4c7fb8e.

--- KB-db/mongo-restore.sh
+++ KB-db/mongo-restore.sh
@@ -8,10 +8,7 @@ chmod 777 /var/log/mongodb.log
 # Start mongodb with logging
 # --logpath    Without this mongod will output all log information to the standard output.
 # --logappend  Ensure mongod appends new entries to the end of the logfile. We create it first so that the below tail always finds something
-
-/entrypoint.sh
-
-mongod --logpath /var/log/mongodb.log --logappend &
+/entrypoint.sh mongod --logpath /var/log/mongodb.log --logappend &
 
 # Wait until mongo logs that it's ready (or timeout after 60s)
 COUNTER=0
